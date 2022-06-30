95866be33e11842c2a2f5da8a817729cf6f7f167
Kerem Kaya <kaya.kerrem@gmail.com>
2022-06-30 13:01:59 +0300

    fix major mistake

--- app.py
+++ app.py
@@ -266,7 +266,7 @@ def employee_talent_get():
         return jsonify({'message': 'Something went wrong'}), 500
 
 
-@app.route('/employee/talent-market/<student-email>', methods=['POST'])
+@app.route('/employee/talent-market/<student_email>', methods=['POST'])
 @jwt_required()
 def employee_talent_add(student_email):
     try:
