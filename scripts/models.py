fa4dd64661caae19c6ebb264540ed7f304906df3
Ahmet Yıldız <yildizah@mef.edu.tr>
2022-08-04 17:15:25 +0300

    Minor changes models

--- scripts/models.py
+++ scripts/models.py
@@ -1,4 +1,3 @@
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.dialects.postgresql import JSON as pg_JSON
 from datetime import datetime
 from scripts.util import db
@@ -58,7 +57,7 @@ class Students(db.Model):
     grad_date       = db.Column(db.DateTime, nullable=True)
     date            = db.Column(db.DateTime, nullable=False)
 
-    favourites      = db.relationship('Favourites', backref='students_ref', lazy=True)
+    favourites      = db.relationship('Favourites', backref='students', lazy=True)
 
     def __init__(self, email, password, name, surname):
         self.email = email
@@ -80,14 +79,17 @@ class Companies(db.Model):
     company_users = db.Column(db.ARRAY(db.String(100)), nullable=True)
     date = db.Column(db.DateTime, nullable=False)
 
-    employees = db.relationship('Employees', backref='companies_ref', lazy=True)
-    favourites = db.relationship('Favourites', backref='companies_ref', lazy=True)
+    employees = db.relationship('Employees', backref='companies', lazy=True)
+    favourites = db.relationship('Favourites', backref='companies', lazy=True)
 
     def __init__(self, company_name, special_id, company_users):
         self.company_name = company_name
         self.special_id = special_id
         self.company_users = company_users
         self.date = datetime.now()
+
+    def __repr__(self) -> str:
+        return f'<Company "{self.company_name}">'
 
     def add_user(self, user):
         self.company_users.append(user)
@@ -104,26 +106,30 @@ class Employees(db.Model):
     email           = db.Column(db.String(120), unique=True, nullable=False)
     phone           = db.Column(db.String(25), nullable=True) # the length may be shortened
     password        = db.Column(db.String(255), nullable=False)
-    company_name    = db.Column(db.String(255), nullable=True)
     t_c             = db.Column(db.Boolean, nullable=True)
     t_c_date        = db.Column(db.DateTime, nullable=True)
     sign_up_date    = db.Column(db.DateTime, nullable=True)
     duration        = db.Column(db.String(255), nullable=True)
     fav_amount      = db.Column(db.Integer, nullable=True)
-    pool_amount     = db.Column(db.Integer, nullable=True)
-    company         = db.Column(db.ForeignKey('companies.special_id'), nullable=False)
+    company_id      = db.Column(db.Integer, db.ForeignKey('companies.id'), nullable=False)
     date            = db.Column(db.DateTime, nullable=False)
 
-    favourites      = db.relationship('Favourites', backref='employees_ref', lazy=True)
+    favourites      = db.relationship('Favourites', backref='employees', lazy=True)
 
-    def __init__(self, name, surname, email, password, company):
+    def __init__(self, name, surname, email, password, company_id):
         self.name = name
         self.surname = surname
         self.email = email
         self.password = password
-        self.company = company
+        self.company_id = company_id
+        self.t_c = False
+        self.duration = 0
+        self.fav_amount = 0
         self.date = datetime.now()
     
+    def __repr__(self):
+        return f'<Employee "{self.name} {self.surname}">'
+
     def to_dict(self):
         return {i.name: getattr(self, i.name) for i in self.__table__.columns if i.name != 'password'}
 
@@ -131,9 +137,9 @@ class Employees(db.Model):
 class Favourites(db.Model):
     __tablename__ = 'favourites'
     id=db.Column(db.Integer,primary_key=True)
-    student_id = db.Column(db.ForeignKey('students.id'), nullable=False)
-    company_name = db.Column(db.ForeignKey('companies.company_name'), nullable=False)
-    employee_email = db.Column(db.ForeignKey('employees.email'), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('students.id'), nullable=False)
+    company_id = db.Column(db.Integer, db.ForeignKey('companies.id'), nullable=False)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'), nullable=False)
     date = db.Column(db.DateTime)
 
     def __init__(self, student_id, company_name, employee_email):
@@ -143,7 +149,7 @@ class Favourites(db.Model):
         self.date = datetime.now()
 
 
-# TODO: Poola göre düzenle
+"""# TODO: Poola göre düzenle
 class Pools(db.Model):
     __tablename__ = 'pools'
     id=db.Column(db.Integer,primary_key=True)
@@ -157,7 +163,7 @@ class Pools(db.Model):
         self.company_name = company_name
         self.employee_email = employee_email
         self.date = datetime.now()
-
+"""
 
 class Programs(db.Model):
     __tablename__= 'programs'
