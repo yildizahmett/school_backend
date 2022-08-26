457214f2f72bbe52c95a704deef5c14115d5e105
Ahmet Yıldız <yildizah@mef.edu.tr>
2022-08-26 12:36:39 +0300

    Add try block for queues

--- receiver.py
+++ receiver.py
@@ -15,34 +15,44 @@ def main():
 
     def callback_std_mail(ch, method, properties, body):
         print(" [x] Received %r" % body)
-        mail = eval(body.decode())
-        
-        email = mail['email']
-        subject = mail['subject']
-        body = mail['body']
-
-        send_mail(email, subject, body)
+        try:
+            mail = eval(body.decode())
+            
+            email = mail['email']
+            subject = mail['subject']
+            body = mail['body']
+            send_mail(email, subject, body)
+        except Exception as e:
+            print(e)
+            pass
         time.sleep(1)
 
     def callback_emp_mail(ch, method, properties, body):
         print(" [x] Received %r" % body)
-        mail = eval(body.decode())
-        
-        email = mail['email']
-        subject = mail['subject']
-        body = mail['body']
+        try:
+            mail = eval(body.decode())
+            
+            email = mail['email']
+            subject = mail['subject']
+            body = mail['body']
+            send_mail(email, subject, body)
 
-        send_mail(email, subject, body)
+        except Exception as e:
+            print(e)
+            pass
         time.sleep(1)
 
     def callback_search_log(ch, method, properties, body):
         print(" [x] Received %r" % body)
-        info = eval(body.decode())
+        try:
+            info = eval(body.decode())
 
-        selected_filter = info['selected_filter']
-        filtered_by = info['filtered_by']
-
-        search_talent_log(selected_filter, filtered_by)
+            selected_filter = info['selected_filter']
+            filtered_by = info['filtered_by']
+            search_talent_log(selected_filter, filtered_by)
+        except Exception as e:
+            print(e)
+            pass
         time.sleep(1)
 
     channel.basic_consume(queue='student_mail_sending', on_message_callback=callback_std_mail, auto_ack=True)
